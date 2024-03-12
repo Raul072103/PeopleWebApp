7f547bc77905c37ea70571b831915ed9244b8d92
raul <raulandrei2017@yahoo.com>
2024-03-12 20:04:36 +0200

    Tried some things in springboot

---
+++ src/main/java/com/raul/people_db/PeopleDbApplication.java
@@ -0,0 +1,13 @@
+package com.raul.people_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleDbApplication.class, args);
+	}
+
+}
